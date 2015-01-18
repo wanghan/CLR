cd2a1bd10e32091c46b48c68820b81f8fc1b22a2
Han Wang <hanwang.work@gmail.com>
2015-01-18 21:43:16 +0800

    [Add] Sample code for thread pool and execution context.

---
+++ CLR/ThreadDemo/ThreadPoolSample.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+
+namespace ThreadDemo
+{
+    class ThreadPoolSample
+    {
+        static void Main(string[] args)
+        {
+            ExecutionContextsDemo();
+        }
+
+        private static void ThreadPoolDemo()
+        {
+            Console.WriteLine("Main thread: starting a dedicated thread to do an async opertaion");
+
+            ThreadPool.QueueUserWorkItem(ComputeBoundOp, 5);
+
+            Console.WriteLine("Main thread: Doing other work here...");
+
+            Thread.Sleep(10000);
+
+            Console.WriteLine("Hit <Enter> to end this program.");
+            Console.ReadLine();
+        }
+
+        private static void ComputeBoundOp(object state)
+        {
+            Console.WriteLine("In compute-BoundOp: state={0}", state);
+
+            Thread.Sleep(1000);
+        }
+
+        private static void ExecutionContextsDemo()
+        {
+            CallContext.LogicalSetData("Name", "Han");
+
+            ThreadPool.QueueUserWorkItem(state => Console.WriteLine("Name={0}", CallContext.LogicalGetData("Name")));
+
+            ExecutionContext.SuppressFlow();
+
+            ThreadPool.QueueUserWorkItem(state => Console.WriteLine("Name={0}", CallContext.LogicalGetData("Name")));
+
+            ExecutionContext.RestoreFlow();
+
+            Thread.Sleep(10000);
+
+            Console.WriteLine("Hit <Enter> to end this program.");
+            Console.ReadLine();
+        }
+    }
+}
