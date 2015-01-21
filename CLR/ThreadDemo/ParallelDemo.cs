ca492e0ae874b03a7dc497294b5e3e17ce811821
Han Wang <hanwang.work@gmail.com>
2015-01-21 15:33:12 +0800

    [Add] Parallel Demo.

---
+++ CLR/ThreadDemo/ParallelDemo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadDemo
+{
+    internal static class ParallelDemo
+    {
+        public static Int64 DirecrtoryBytes(string path, string searchPattern, SearchOption searchOption)
+        {
+            var files = Directory.EnumerateFiles(path, searchPattern, searchOption);
+            Int64 masterLength = 0;
+
+            ParallelLoopResult result = Parallel.ForEach<string, Int64>(
+                files,
+
+                () =>
+                {
+                    return 0;
+                },
+
+                (file, loopState, index, taskLocalTotal) =>
+                {
+                    Int64 fileLength = 0;
+                    try
+                    {
+                        using (FileStream fs = File.OpenRead(file))
+                        {
+                            fileLength = fs.Length;
+                        }
+                    }
+                    catch (IOException) { }
+                    return taskLocalTotal + fileLength;
+                },
+
+                taskLocalTotal =>
+                {
+                    // MasterLength should be updated in a thread-safe way.
+                    Interlocked.Add(ref masterLength, taskLocalTotal);
+                }
+                );
+
+            return masterLength;
+        }
+    }
+}
